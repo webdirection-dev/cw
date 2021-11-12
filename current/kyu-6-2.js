120a28da14e4f7d678446825a3cceb900136d24c
Anton Petrov <52886942+webdirection-dev@users.noreply.github.com>
2021-11-12 04:05:16 +0300

    +

--- current/kyu-6-2.js
+++ current/kyu-6-2.js
@@ -1,3 +1,28 @@
+// function generateShape(integer){
+//     const arr = [];
+//
+//     for (let i = 0; i < integer; i++) {
+//         const out = [];
+//         for (let i = 0; i < integer; i++) {
+//             out.push('+')
+//         }
+//             arr.push(out.join(''))
+//     }
+//
+//     return arr.join('\n');
+// }
+// console.log(generateShape(5))
 
-let s= 2
-console.log(s)
+// function sumOrProduct(array, n) {
+//     let max = 0, min = 1;
+//
+//     array = array
+//         .sort((a, b) => b - a)
+//         .forEach((item, index) => {
+//             if (index < n) max += item;
+//             if (index > array.length - n - 1) min *= item;
+//         });
+//
+//     return max > min ? 'sum' : min > max ? 'product' : 'same';
+// }
+// console.log(sumOrProduct([10, 41, 8, 16, 20, 36, 9, 13, 20], 3))
